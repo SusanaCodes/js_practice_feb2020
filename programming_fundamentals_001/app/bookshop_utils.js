598a9180df5fb39b0c61a6d86d272c975ce4ab18
Susana Mncwabe <susanamncwabe@outlook.com>
2020-03-07 13:36:21 +0000

    Checking why function is running by every test

---
+++ programming_fundamentals_001/app/bookshop_utils.js
@@ -0,0 +1,30 @@
+function addVAT(price) {
+    constant tax = 0.2 * price;
+    return price + (0.2 * price);
+}
+function getFullName(name, lastName) {
+    return name + " " + lastName;
+}
+function makeHalfPrice(price) {
+    return (price/2);
+}
+function countBooks(listOfBooks){
+    return (listOfBooks.length);
+}
+
+
+
+module.exports = {
+    addVAT,
+    getFullName,
+    makeHalfPrice,
+    countBooks,
+    isInStock,
+    noInStock,
+    getTotalOrderPrice
+};
+
+function countBooks(){
+    return (Books.length);
+}
+
